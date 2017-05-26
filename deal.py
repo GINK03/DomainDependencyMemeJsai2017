3c3fc9cb2a1b1d265bd973b7c5ac54589a48d860
G K <gim.kobayashi@gmail.com>
2017-05-26 23:10:42 +0900

    エロマンガ先生

---
+++ deal.py
@@ -0,0 +1,25 @@
+import os
+import glob
+import math
+import sys
+import re
+import MeCab
+def step1():
+  m = MeCab.Tagger("-Owakati")
+  names  = glob.glob('../YahooNewsScraper/output/*/*')
+  alllen = len(names)
+  for en, name in enumerate(names):
+    if en%500 == 0:
+      print(en, alllen, name)
+    with open(name, 'r') as f, open("yahoo.news.txt", "a") as w:
+      for line in f:
+        line  = line.strip()
+        lines = filter(lambda x:x!="", re.split(r"(\n)", line) )
+        for e in lines:
+          wakati = m.parse(e).strip()
+          w.write( wakati + "\n" )
+          #print(wakati + "\n")
+
+if __name__ == '__main__':
+  if '--step1' in sys.argv:
+    step1()
